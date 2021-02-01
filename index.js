571807576881c157dfc1c6c97296f39fcca075fc
prince961 <prince.prince961@gmail.com>
2021-02-01 21:09:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const routes =require("./routes/api");
+const routes =require("./Routes/api");
 const mongoose = require('mongoose');
 
 const app = express();
