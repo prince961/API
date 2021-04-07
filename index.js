8d37622af4306ebd617bf2a7f916ce09a6171b04
Nitin <nitin3emails@gmail.com>
2021-04-07 18:27:14 +0530

    Added login method

--- index.js
+++ index.js
@@ -3,60 +3,71 @@ var cors = require("cors");
 const bodyParser = require("body-parser");
 const orderRoutes = require("./Routes/orders.route");
 const pincodeRoute = require("./Routes/pincode.route");
+const authRoute = require("./Routes/auth.route");
 const mongoose = require("mongoose");
-var LocalStorage = require("node-localstorage").LocalStorage;
-localStorage = new LocalStorage("./scratch");
-const User = require("./Models/user");
+
+const HttpError = require("./models/http-error");
+// var LocalStorage = require("node-localstorage").LocalStorage;
+// localStorage = new LocalStorage("./scratch");
+// const User = require("./Models/user");
 
 const app = express();
 app.use(cors());
 
-var auth = function (req, res, next) {
-  var authorization = req.headers.authorization;
-  console.log(authorization);
-  if (authorization == undefined) {
-    console.log("please use basic Authorization");
-    res.send("please use basic Authorization");
-  } else {
-    var userPass = authorization.split(" ")[1];
-    var plainText = Buffer.from(userPass, "base64").toString("ascii");
-    var userName = plainText.split(":")[0];
-    var password = plainText.split(":")[1];
-    localStorage.setItem("userName", userName);
-    console.log(
-      "userName from local storage" + localStorage.getItem("userName")
-    );
-    var dbUserPassword;
+// var auth = function (req, res, next) {
+//   var authorization = req.headers.authorization;
+//   console.log(authorization);
+//   if (authorization == undefined) {
+//     console.log("please use basic Authorization");
+//     res.send("please use basic Authorization");
+//   } else {
+//     var userPass = authorization.split(" ")[1];
+//     var plainText = Buffer.from(userPass, "base64").toString("ascii");
+//     var userName = plainText.split(":")[0];
+//     var password = plainText.split(":")[1];
+//     localStorage.setItem("userName", userName);
+//     console.log(
+//       "userName from local storage" + localStorage.getItem("userName")
+//     );
+//     var dbUserPassword;
 
-    //Query DB for credentials
-    User.findOne({ userName: userName }).then(function (dbUser) {
-      localStorage.setItem("userPassword", dbUser.password);
-      dbUserPassword = dbUser.password;
-    });
+//     //Query DB for credentials
+//     User.findOne({ userName: userName }).then(function (dbUser) {
+//       localStorage.setItem("userPassword", dbUser.password);
+//       dbUserPassword = dbUser.password;
+//     });
 
-    console.log("Authentication password is: " + password);
-    console.log("dbUser: " + dbUserPassword);
-    if (password === localStorage.getItem("userPassword")) {
-      localStorage.setItem("userPassword", "NA");
-      next();
-    } else {
-      res.send("authentication failed");
-      res.status(401);
-    }
-  }
-};
+//     console.log("Authentication password is: " + password);
+//     console.log("dbUser: " + dbUserPassword);
+//     if (password === localStorage.getItem("userPassword")) {
+//       localStorage.setItem("userPassword", "NA");
+//       next();
+//     } else {
+//       res.send("authentication failed");
+//       res.status(401);
+//     }
+//   }
+// };
 
 // using routes
 
 app.use(bodyParser.json());
-app.use(auth);
+// app.use(auth);
 app.use("/api", orderRoutes);
 app.use("/api/pincode", pincodeRoute);
+app.use("/api/auth", authRoute);
 
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  return next(error);
+});
 //error handling middleware
-app.use(function (err, req, res, next) {
-  console.log(err);
-  res.status(422).send("error: " + err);
+app.use(function (error, req, res, next) {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 //port setting
